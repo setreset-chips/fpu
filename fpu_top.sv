06c242ae32de031b023fab7fe15d631171acb71c
Ved Vyas <vedvyas2005@icloud.com>
2024-02-10 17:03:12 -0600

    pipeline config-ish

--- fpu_top.sv
+++ fpu_top.sv
@@ -1,9 +1,20 @@
 module fpu_top (
-	input logic [31:0] instruction,
+	input logic [31:0] instructions,
+	input logic clk,
 	
 	output logic [31:0] fpu_out
 );
 
-// Misc.
+	logic [31:0] regFile [4:0];
 
+// 4-Stage Pipeline
+// Stage 1: Fetch Operands (Only for Memory)
+// Stage 2: Decode
+// Stage 3: Execute
+// Stage 4: Writeback
+	
+	always @ () begin // Stage 1: Fetch Operands
+		
+	end
+	
 endmodule
